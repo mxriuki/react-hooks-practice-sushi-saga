3b7ebe78804f8f6dee1a42fa17bcf9a9754e743b
mxriuki <mwendav37@gmail.com>
2024-08-02 19:25:59 +0300

    challenge

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [budget, setBudget] = useState(100)
+  const [sushis, setSushis] =useState([]);
+
+
+
+  useEffect(() => fetch(API)
+  .then(res => res.json())
+  .then(setSushis), []);
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} eatSushi={eatSushi} />
+      <Table plates={sushis.filter(sushis => sushis.eaten)} budget={budget} />
     </div>
   );
+  function eatSushi(sushi){
+
+    if (budget >= sushi.price){
+      setBudget(budget - sushi.price);
+    const newSushis = sushis.map(s => s.id !== sushi.id ? s : {...s, eaten: true});
+
+    setSushis(newSushis);
+    }
+  }
 }
 
 export default App;
